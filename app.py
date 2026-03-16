f7a873edf5909c33f12d72a384ed9dfde48da309
sHunjang <seunghunJ922@gmail.com>
2026-03-16 17:38:22 +0900

    fix: Disable SSR (Fix Spaces Starting issue)

--- app.py
+++ app.py
@@ -321,5 +321,6 @@ if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        ssr_mode=False
+        ssr_mode=False,
+        share=False,
     )
